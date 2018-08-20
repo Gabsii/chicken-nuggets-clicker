e71d697a1cda68d7d470f119f195223022f42505
luuca <luka.trailovic1@gmail.com>
2018-08-20 21:10:24 +0200

    Revert "Merge branch 'master' of https://github.com/Gabsii/chicken-nuggets-clicker"
    This reverts commit 6369d2eb19781cbcd2d4f63340dc779adad05c0b, reversing
    changes made to 9c894df33a1ffe1201bcac014acfe22ba8ec740f.

--- App.js
+++ App.js
@@ -1,80 +1,46 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
-import {Font} from 'expo';
-import LinearGradient from 'react-native-linear-gradient';
-
 import ChickenButton from './components/ChickenButton';
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#80534E',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexDirection: 'column'
-    },
-    counter: {
-        alignSelf: 'stretch',
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: 'red',
-        minHeight: 80,
-        marginBottom: 50,
-        marginTop: -20
-    }
-});
-
 export default class App extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            counter: 0,
-            velocity: 0,
-            powerUps: {
-                double: null
-            },
-            fontLoaded: false
-        };
-    }
 
-    async componentDidMount() {
-        await Expo.Font.loadAsync({'vcr-osd-mono': require('./assets/fonts/VCR_OSD_MONO.ttf')});
-        this.setState({fontLoaded: true});
-    }
+	constructor() {
+		super();
+		this.state = {
+			counter: 0,
+			velocity: 0,
+			powerUps: {
+				double: null
+			}
+		}
+	}
 
-    click() {
-        this.setState({
-            counter: this.state.counter + 1
-        });
-    }
+	click() {
+		this.setState({
+			counter: this.state.counter + 1
+		});
+	}
 
-    render() {
-        return (<View style={styles.container}>
-            {
-                this.state.fontLoaded
-                    ? (/*<LinearGradient start={{x: 0, y: 0}} end={{x: 1, y: 1}} colors={['transparent', 'rgba(255,255,255,.5)', 'transparent']}>*/
-                        <View style={styles.counter}>
-                            <Text style={{
-                                    fontSize: 20,
-                                    color: '#fff',
-                                    textAlign: 'center',
-                                    fontFamily: 'vcr-osd-mono'
-                                }}>
-                                {this.state.counter}
-                            </Text>
-                            <Text style={{
-                                    fontSize: 16,
-                                    color: 'grey',
-                                    textAlign: 'center',
-                                    fontFamily: 'vcr-osd-mono'
-                                }}>
-                                {this.state.velocity + ' Nuggets/sec'}
-                            </Text>
-                        </View>
-                    /*</LinearGradient>*/)
-                    : null
-            }
-            <ChickenButton click={this.click.bind(this)}/>
-        </View>);
-    }
+	render() {
+		return (<View style={styles.container}>
+			<ChickenButton style={styles.button} click={this.click.bind(this)}/>
+			<Text style={styles.primaryCounter}>{this.state.counter}</Text>
+			<Text style={styles.secondaryCounter}>{this.state.velocity + " Nuggets/sec"}</Text>
+		</View>);
+	}
 }
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+		flexDirection: 'column'
+	},
+	primaryCounter: {
+		fontSize: 20
+	},
+	secondaryCounter: {
+		fontSize: 16
+	}
+});
--- components/ChickenButton.js
+++ components/ChickenButton.js
@@ -1,23 +1,22 @@
-import React from "react";
-import {StyleSheet, TouchableOpacity, View} from "react-native";
+import React from 'react';
+import {StyleSheet, TouchableHighlight, Image, View} from 'react-native';
 
-import LocalImage from "./LocalImage";
-
-const abstd = 10;
 export default class ChickenButton extends React.Component {
-    render() {
-        return (
-            <View style={{flexDirection: "row"}}>
-                <TouchableOpacity onPress={this.props.click.bind(this)}>
-                    <LocalImage
-                        style={{margin: "0, " + abstd}}
-                        source={require("../assets/img/nugget.png")}
-                        originalWidth={2761 - 2 * abstd}
-                        originalHeight={2000}
-                        gap={50}
-                    />
-                </TouchableOpacity>
-            </View>
-        );
-    }
+
+	constructor() {
+		super();
+	}
+
+	render() {
+		return (<TouchableHighlight onPress={this.props.click}>
+			<Image source={require('../img/test.jpg')} style={styles.image}/>
+		</TouchableHighlight>);
+	}
 }
+
+const styles = StyleSheet.create({
+	image: {
+		height: 250,
+		width: 250
+	}
+});
